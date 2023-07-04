eed3d343814ee10646e0f1fb52e9ff9a37a1b309
Anuj <anujspmishra@gmail.com>
2023-07-04 17:47:57 +0530

    This is digital watch using shell scripting

---
+++ Digital_watch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BBlack='\033[1;30m'       # Black
+Red=$'\e[1;31m'         # Red
+Green=$'\033[1;32m'       # Green
+BYellow=$'\033[1;33m'      # Yellow
+BBlue=$'\033[1;34m'        # Blue
+BPurple=$'\033[1;35m'      # Purple
+BCyan=$'\033[1;36m'        # Cyan
+BWhite=$'\033[1;37m'       # White
+
+while true
+do
+    clear
+    echo $Green $(date +%T)
+    sleep 1s
+done
