3aa57d01f727babdf2310fbaee15772bd4e95b69
Anuj Mishra <113639224+Anuj-Mishra8853@users.noreply.github.com>
2023-07-04 20:55:43 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Please enter the first number:"
+echo "Enter the first number: "
 read num1
 
-echo "Please enter the second number:"
+echo "Enter the second number: "
 read num2
 
-echo "Please enter the operation (+, -, *, /):"
+echo "Enter the operation (+, -, *, /):"
 read op
 
 if [ "$op" == "+" ]; then
